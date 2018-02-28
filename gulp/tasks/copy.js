0ac10b796e8097c00fbf57fb069e830896e39d35
jon <jgkerkhoven@gmail.com>
2018-02-28 10:57:55 +0100

    remove gulpfiles, create src folder

--- gulp/tasks/copy.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var copy = require('gulp-contrib-copy');
- 
-gulp.task('copy', function() {
-  gulp.src(['src/**/*', '!./src/scss/**'])
-    .pipe(copy())
-    .pipe(gulp.dest('build/'));
-});
--- gulp/tasks/watch.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('watch', ['sass'], function (){
-	gulp.watch('src/scss/**/*.scss', ['sass'])
-	gulp.watch('src/sections/**/*.php', ['copy'])
-	gulp.watch('src/js/*.js', ['babel']);
-})
